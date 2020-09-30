03bf924a223675892576bbe56230612fc389719f
SimoSultan <simosultan2020@gmail.com>
2020-09-30 23:18:38 +1000

    imported gsap and animated profile image on button click

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Navigation from '../other/Navigation'
 import profileImage from '../../img/v1_transparent.png'
 // import { profileImage } from 'images/v1_transparent.png'
-import "../../stylesheets/Home.css";
 import { aboutMe } from './aboutMe'
 
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+
+import { TweenMax } from 'gsap';
 
 
 const useStyles = makeStyles(() => ({
@@ -21,6 +22,9 @@ const useStyles = makeStyles(() => ({
     margin: 0,
     padding: 0,
     background: '#F2F3F5'
+  },
+  profileImage: {
+    width: '90%',
   }
 }));
 
@@ -32,13 +36,33 @@ const AboutMe = () => (
 )
 
 
+
 function Home() {
   const classes = useStyles();
+  const profImg = useRef(null);
+
   const [showText, setShowText] = useState(false)
+  const [animation, setAnimation] = useState(null)
+
+  useEffect(() => {
+    setAnimation(
+      TweenMax.to(profImg.current, 1, {x: 100}).pause()
+    )
+  }, [])
 
   function showHideAboutMeText() {
-    (showText) ? setShowText(false) : setShowText(true)
+    if (showText) { 
+      setShowText(false) 
+      animation.reverse()
+    } else {
+      setShowText(true)
+      animation.play()
+    }
   }
+
+  // function showHideAboutMeText() {
+  //   (showText) ? setShowText(false) : setShowText(true)
+  // }
 
   return (
 
@@ -47,24 +71,31 @@ function Home() {
 
       <Container className={classes.container}>
 
-        <Box>
-          <Typography variant="h1" gutterBottom>
-            simon
-          </Typography>
-          <Typography variant="h1" gutterBottom>
-            curran.
-          </Typography>
-          <Button onClick={showHideAboutMeText}>
-            <Typography variant="body1" gutterBottom>
-              who?
+        <Grid container direction="row">
+
+          <Grid item direction="column" xs={3}>
+            <Typography variant="h1" gutterBottom>
+              simon
             </Typography>
-          </Button>
+            <Typography variant="h1" gutterBottom>
+              curran.
+            </Typography>
+            <Button onClick={showHideAboutMeText}>
+              <Typography variant="body1" gutterBottom>
+                who?
+              </Typography>
+            </Button>
+          </Grid>
 
-          { showText ? <AboutMe /> : null }
+          <Grid item direction ="column" xs={3}>
+            { showText ? <AboutMe /> : null }
+          </Grid>
 
-        </Box>
-
-        <img id="home-img" src={profileImage} alt="profile"/>
+          <Grid item direction="column" xs={6}>
+            <img ref={profImg} className={classes.profileImage} src={profileImage} alt="profile"/>
+            {/* <ProfileImage /> */}
+          </Grid>
+        </Grid>
 
       </Container>
     </div>
